3a474e88ce11a45c596bd263f9ca793eec93b8b3
LanceAve <lanceave@protonmail.com>
2025-05-13 16:24:55 -0400

    encapsulate MongoDB message logic

--- websocket-app/server/messageService.js
+++ websocket-app/server/messageService.js
@@ -1,1 +1,13 @@
 // messageService.js
+const Message = require('./messageModel');
+
+async function saveMessage({ username, message, clientType }) {
+  const newMsg = new Message({ username, message, clientType });
+  return await newMsg.save();
+}
+
+async function getRecentMessages(limit = 10) {
+  return await Message.find().sort({ timestamp: -1 }).limit(limit).lean();
+}
+
+module.exports = { saveMessage, getRecentMessages };
