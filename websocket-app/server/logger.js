b085fd7d78ce81c76ac686a652ffab990add7f69
LanceAve <lanceave@protonmail.com>
2025-05-13 16:26:09 -0400

    add centralized logger for structured console output

--- websocket-app/server/logger.js
+++ websocket-app/server/logger.js
@@ -1,1 +1,6 @@
 // logger.js
+const log = (...args) => console.log('[INFO]', ...args);
+const warn = (...args) => console.warn('[WARN]', ...args);
+const error = (...args) => console.error('[ERROR]', ...args);
+
+module.exports = { log, warn, error };
