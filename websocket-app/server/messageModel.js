669813c39765f86bf8457a6c13ffbafeb8c3f903
LanceAve <lanceave@protonmail.com>
2025-05-13 16:24:19 -0400

    add mongoose schema for chat messages

--- websocket-app/server/messageModel.js
+++ websocket-app/server/messageModel.js
@@ -1,1 +1,14 @@
 // messageModel.js
+const mongoose = require('mongoose');
+
+// Define the message schema
+const messageSchema = new mongoose.Schema({
+    username: { type: String, required: true },
+    message: { type: String, required: true },
+    timestamp: { type: Date, default: Date.now },
+    clientType: { type: String, enum: ['Swift', 'HTML', 'Other'], default: 'Other' }
+    });
+    
+    // Create the message model
+    const Message = mongoose.model('Message', messageSchema);
+    module.exports = Message;
